1a0bed365bbc9567a2fc9c61f74ba3e8e1056d7f
Yasitha Silva <yasithamadhuranga@gmail.com>
2025-03-03 16:54:39 +0530

    login done
    changes some *body margin and border box attributes.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import GoogleIcon from "../assets/icons/login/google.png";
+import AppleIcon from "../assets/icons/login/mac-os-logo.png";
+import { Box, Typography } from "@mui/material";
+import { ReactComponent as BasementPark } from "../assets/img/Parking-bro.svg";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [mobile, setMobile] = useState("");
 
-export default Login
+  const handleSubmit = () => {
+    console.log("Mobile Number Submitted:", mobile);
+  };
+
+  return (
+    <>
+      <div class="full-height-box">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+          }}>
+          <BasementPark width="300px" height="300px" />
+        </Box>
+        <Typography
+          sx={{
+            textAlign: "center",
+            color: "#484848",
+            fontFamily: "Inter, sans-serif",
+            marginBottom: "72px",
+            fontSize: "48px",
+            fontWeight: "bold",
+          }}>
+          Welcome to Parkspace!
+        </Typography>
+
+        <TextField
+          label="Mobile Number"
+          variant="outlined"
+          fullWidth
+          type="tel"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+          sx={{ marginBottom: 2 }}
+        />
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <Typography
+            sx={{
+              textAlign: "center",
+              color: "#232323",
+              fontFamily: "Inter, sans-serif",
+              margin: "16px",
+              color: "#484848",
+            }}>
+            Or Use
+          </Typography>
+        </Box>
+
+        <div
+          className=""
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "12px",
+            marginBottom: "1rem",
+            width: "100%",
+          }}>
+          <NavLink to="/signup" className="nav-link">
+            <div className="login-icon-box">
+              <img src={GoogleIcon} alt="Google Icon" />
+            </div>
+          </NavLink>
+
+          <NavLink to="/forgot-password" className="nav-link">
+            <div className="login-icon-box">
+              <img src={AppleIcon} alt="Apple Icon" />
+            </div>
+          </NavLink>
+        </div>
+
+        <Button
+          sx={{ marginTop: "36px" }}
+          variant="contained"
+          color="primary"
+          className="btn-primary"
+          fullWidth
+          onClick={handleSubmit}>
+          Submit
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
