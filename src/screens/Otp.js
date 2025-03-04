6f5e3f393cf75945b7616d29279c4a874aa9b5a4
Chathula Shashan <dcswanniarachchi@students.nsbm.ac.lk>
2025-03-05 04:49:36 +0530

    Virtual Keyboard added

--- src/screens/Otp.js
+++ src/screens/Otp.js
@@ -3,15 +3,41 @@ import { Box, Typography, TextField, Button, Radio, RadioGroup, FormControlLabel
 import BackIcon from "../assets/icons/common/caret-left.svg";
 import VisaIcon from "../assets/icons/cardpay/visa.png";
 import MastercardIcon from "../assets/icons/cardpay/master.png";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 const EnterCardDetails = () => {
-  const [cardNumber, setCardNumber] = useState("");
-  const [expiry, setExpiry] = useState("");
-  const [cvn, setCvn] = useState("");
+  // State to store card details
+  const [cardDetails, setCardDetails] = useState({ number: "", expiry: "", cvn: "" });
+  const [inputName, setInputName] = useState(""); // Track which input is focused
   const [showKeyboard, setShowKeyboard] = useState(false);
 
+  // Function to handle input focus
+  const handleInputFocus = (input) => {
+    setInputName(input);
+    setShowKeyboard(true);
+  };
+
+  // Function to handle input change
   const handleInputChange = (setter) => (event) => {
     setter(event.target.value);
+  };
+
+  // Function to handle keyboard changes
+  const handleKeyboardChange = (input) => {
+    if (inputName === "number") {
+      setCardDetails({ ...cardDetails, number: input });
+    } else if (inputName === "expiry") {
+      setCardDetails({ ...cardDetails, expiry: input });
+    } else if (inputName === "cvn") {
+      setCardDetails({ ...cardDetails, cvn: input });
+    }
+  };
+
+  // Handle payment
+  const handlePayment = () => {
+    console.log("Proceeding with payment for:", cardDetails);
+    // Add your payment logic here
   };
 
   return (
@@ -44,9 +70,9 @@ const EnterCardDetails = () => {
       <TextField
         fullWidth
         placeholder="1234 1234 1234 1234"
-        value={cardNumber}
-        onFocus={() => setShowKeyboard(true)}
-        onChange={handleInputChange(setCardNumber)}
+        value={cardDetails.number}
+        onFocus={() => handleInputFocus("number")}
+        onChange={handleInputChange((value) => setCardDetails({ ...cardDetails, number: value }))}
         sx={{ mt: 1 }}
       />
 
@@ -56,16 +82,16 @@ const EnterCardDetails = () => {
         <TextField
           fullWidth
           placeholder="Expiry"
-          value={expiry}
-          onFocus={() => setShowKeyboard(true)}
-          onChange={handleInputChange(setExpiry)}
+          value={cardDetails.expiry}
+          onFocus={() => handleInputFocus("expiry")}
+          onChange={handleInputChange((value) => setCardDetails({ ...cardDetails, expiry: value }))}
         />
         <TextField
           fullWidth
           placeholder="CVN"
-          value={cvn}
-          onFocus={() => setShowKeyboard(true)}
-          onChange={handleInputChange(setCvn)}
+          value={cardDetails.cvn}
+          onFocus={() => handleInputFocus("cvn")}
+          onChange={handleInputChange((value) => setCardDetails({ ...cardDetails, cvn: value }))}
         />
       </Box>
 
@@ -75,16 +101,33 @@ const EnterCardDetails = () => {
         <span>RS. 2375</span>
       </Typography>
 
+
+
+
+           {/* Virtual Keyboard */}
+      {showKeyboard && (
+        <Keyboard
+          onChange={handleKeyboardChange}
+          inputName={inputName}
+          layoutName="default"
+
+          
+        />
+
+
+      )}
+
       {/* Proceed Button */}
       <Button
         variant="contained"
         fullWidth
         sx={{ mt: 3, backgroundColor: "#28a745", color: "white", fontSize: "16px", padding: "12px" }}
+        onClick={handlePayment}
       >
         Proceed Payment
       </Button>
 
-      
+ 
     </Box>
   );
 };
